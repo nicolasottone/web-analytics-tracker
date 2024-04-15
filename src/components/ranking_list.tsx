0f89358a22fddc8cd3320ab93a60842d539c5b27
Nicolas Ottone <nicolasottone@hotmail.com>
2024-04-15 03:53:43 -0300

    RankingList component created

---
+++ src/components/ranking_list.tsx
@@ -0,0 +1,30 @@
+import { valueFormatter } from '@/lib/utils'
+import { BarList, Card } from '@tremor/react'
+
+interface RankingListProps {
+  title: string
+  metric: string
+  color?: string
+  items: {
+    name: string
+    value: number
+  }[]
+}
+
+export default function RankingList({ title, metric, items, color }: RankingListProps) {
+  return (
+    <>
+      <Card className="p-0 sm:mx-auto sm:max-w-lg">
+        <div className="flex items-center justify-between border-b border-tremor-border p-6 dark:border-dark-tremor-border">
+          <p className="font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">{title}</p>
+          <p className="text-tremor-label font-medium uppercase text-tremor-content dark:text-dark-tremor-content">
+            {metric}
+          </p>
+        </div>
+        <div className={`overflow-hidden p-6`}>
+          <BarList color={color ? color : 'blue'} data={items} valueFormatter={valueFormatter} />
+        </div>
+      </Card>
+    </>
+  )
+}
